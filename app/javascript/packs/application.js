bc8d61ebca5d0762012bca7f86715838974e8435
xp3rt4g <machesys@gmail.com>
2021-01-19 19:54:56 +0100

    fixed

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,10 +2,6 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-//= require jquery.ui.widget
-//= require z.jquery.fileupload
-//= require jquery-fileupload/basic
-
 
 require("@rails/ujs").start()
 require("turbolinks").start()
@@ -14,12 +10,6 @@ require("channels")
 require("packs/jquery.fileupload")
 require("packs/jquery.ui.widget")
 require("packs/widget")
-
-import $ from 'jquery';
-
-global.$ = $
-global.jQuery = $
-
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
