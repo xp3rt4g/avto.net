169789862233cb10f156981cd979e32f91af528b
xp3rt4g <machesys@gmail.com>
2021-01-22 23:38:43 +0100

    still trying with google login

--- Gemfile
+++ Gemfile
@@ -9,7 +9,7 @@ gem 'rails', '~> 6.0.3', '>= 6.0.3.4'
 gem 'pg'
 #devise 
 gem 'devise', :git => "https://github.com/heartcombo/devise.git", ref: '8bb358cf80a632d3232c3f548ce7b95fd94b6eb2'
-gem 'omniauth-facebook'
+gem 'omniauth-google-oauth2'
 
 #imguploads
 gem 'carrierwave', "0.10.0"
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,22 +1,24 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  
   has_many :cars
   belongs_to :account_type
   belongs_to :town
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, 
-         :omniauthable, omniauth_providers: [:facebook]
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:google_oauth2]
 
   def self.from_omniauth(auth)
-  where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-    user.email = auth.info.email
-    user.password = Devise.friendly_token[0, 20]
-    user.name = auth.info.name   # assuming the user model has a name
-    # If you are using confirmable and the provider(s) you use validate emails, 
-    # uncomment the line below to skip the confirmation emails.
-    # user.skip_confirmation!
+    user = User.find_by(email: auth.info.email)
+    if user
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.save
+    else
+      user = User.where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+        user.email = auth.info.email
+        user.password = Devise.friendly_token[0,20]
+      end
+    end
+    user
   end
-end
 end
