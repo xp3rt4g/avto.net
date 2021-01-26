86202cfbe9ad00902b41a206529872f59ee2b7dd
xp3rt4g <machesys@gmail.com>
2021-01-26 21:57:29 +0100

    added forgot password

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -58,6 +58,7 @@ class UsersController < ApplicationController
   # DELETE /users/1
   # DELETE /users/1.json
   def destroy
+    Car.where(user_id: @user.id).destroy
     @user.destroy
     respond_to do |format|
       format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
