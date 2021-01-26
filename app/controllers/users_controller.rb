8275d47b2a74bf905975f16155fee16a3b2faba1
xp3rt4g <machesys@gmail.com>
2021-01-26 22:22:47 +0100

    added so when you delete an user his posts are deleted as well

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -58,7 +58,10 @@ class UsersController < ApplicationController
   # DELETE /users/1
   # DELETE /users/1.json
   def destroy
-    Car.where(user_id: @user.id).destroy
+    @cars = Car.where(user_id: @user.id).destroy
+    @cars.each do |car|
+      car.destroy
+    end
     @user.destroy
     respond_to do |format|
       format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
