72c9b2a8b3dd19544ab52ba276f2d85d5e4d4978
xp3rt4g <machesys@gmail.com>
2021-01-26 21:04:09 +0100

    added confirmable

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   has_many :cars
   belongs_to :account_type
   belongs_to :town
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, :confirmable, 
          :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: %i[facebook google_oauth2]
 
   def self.from_omniauth(auth)
